c6a7a70614c690f85be36a1c943b343d8014ab9b
jtferri <jferri17@gmail.com>
2025-02-27 17:44:15 -0500

    Add files via upload

---
+++ sankeywebpage.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import requests
+
+# Display title
+st.title('Sankey Graphic')
+
+# Raw URL to the HTML file on GitHub
+html_url = "https://raw.githubusercontent.com/jtferri/public-repository/refs/heads/main/sankey.html"
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8784a60de4602804930b19c2c334280018878260
+# Fetch the HTML content
+response = requests.get(html_url)
+
+if response.status_code == 200:
+    plot_html = response.text
+    st.components.v1.html(plot_html, height=600)
+else:
+    st.error("Failed to load the HTML file from GitHub.")
